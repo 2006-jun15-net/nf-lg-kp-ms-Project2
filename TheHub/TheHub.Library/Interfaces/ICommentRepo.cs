41912085a0cd492e20b004d6f55892ef4c9b2601
kirtipatel98 <kirti.bapa@gmail.com>
2020-07-18 10:17:54 -0700

    forgot to push comment interface

--- TheHub/TheHub.Library/Interfaces/ICommentRepo.cs
+++ TheHub/TheHub.Library/Interfaces/ICommentRepo.cs
@@ -1,7 +1,18 @@
 ﻿using System;
+using TheHub.Library.Model;
+
 namespace TheHub.Library.Interfaces
 {
     public interface ICommentRepo
     {
+        Comment GetById(int id);
+
+        Comment GetByReviewerId(int id);
+
+        void Add(Comment comment);
+
+        void Update(Comment comment);
+
+        void Delete(int id);
     }
 }
