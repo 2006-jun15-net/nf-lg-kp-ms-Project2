858fd30b3125f0e2a36f175b2803df354fe7f5c4
Luis Gomez <luey714@gmail.com>
2020-07-20 10:45:57 -0700

    fixed reccursion in review library model

--- TheHub/TheHub.Library/Model/Review.cs
+++ TheHub/TheHub.Library/Model/Review.cs
@@ -6,59 +6,65 @@ namespace TheHub.Library.Model
 {
     public class Review
     {
-        public int ReviewId
-        {
-            get => ReviewId;
-            set
-            {
-                if (value == 0)
-                {
-                    throw new ArgumentException("Review ID cannot be 0", nameof(value));
-                }
-                ReviewId = value;
-            }
-        }
-        public string Content 
-        {
-            get => Content;
-            set
-            {
-                if (value == "")
-                {
-                    throw new ArgumentNullException("Contents of review may not be empty", nameof(value));
-                }
-                Content = value;
-            }
-        }
-        public int MediaId
-        {
-            get => MediaId;
-            set
-            {
-                if (value == 0)
-                {
-                    throw new ArgumentException("Media ID cannot be 0", nameof(value));
-                }
-                MediaId = value;
-            }
-        }
-        public int UserId
-        {
-            get => UserId;
-            set
-            {
-                if (value == 0)
-                {
-                    throw new ArgumentException("Review ID cannot be 0", nameof(value));
-                }
-                UserId = value;
-            }
-        }
+        private int _reviewId;
+        private string _content;
+        private int _mediaId;
+        private int _userId;
+        
         public DateTime ReviewDate { get; set; }
         public int? Likes { get; set; }
         public int? Rating { get; set; }
         public List<Comment> comments = new List<Comment>();
         public List<User> Likers = new List<User>();
 
+
+        public int ReviewId
+        {
+            get => _reviewId;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Review ID cannot be 0", nameof(value));
+                }
+                _reviewId = value;
+            }
+        }
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentNullException("Contents of review may not be empty", nameof(value));
+                }
+                _content = value;
+            }
+        }
+        public int MediaId
+        {
+            get => _mediaId;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Media ID cannot be 0", nameof(value));
+                }
+                _mediaId = value;
+            }
+        }
+        public int UserId
+        {
+            get => _userId;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Review ID cannot be 0", nameof(value));
+                }
+                _userId = value;
+            }
+        }
     }
 }
